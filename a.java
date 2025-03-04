8a752abf0bc722f65de927e4c154f677049e267b
Smita Rao <77341182+smitarao25@users.noreply.github.com>
2025-03-04 18:45:08 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,24 +1,17 @@
 public class Main {
-
   public static void main(String[] args) {
-
     int num = 29;
     boolean flag = false;
-
     // 0 and 1 are not prime numbers
     if (num == 0 || num == 1) {
         flag = true;
     }
-
     for (int i = 2; i <= num / 2; ++i) {
-
-     
       if (num % i == 0) {
         flag = true;
         break;
       }
     }
-
     if (!flag)
       System.out.println(num + " is a prime number.");
     else
