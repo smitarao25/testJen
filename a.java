6be725ed2503ab6b816550a20a6ccb43d69093fc
Smita Rao <77341182+smitarao25@users.noreply.github.com>
2025-03-11 17:17:14 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,19 +1,6 @@
 public class a {
   public static void main(String[] args) {
-    int num = 29;
-    boolean flag = false;
-    if (num == 0 || num == 1) {
-        flag = true;
-    }
-    for (int i = 2; i <= num / 2; ++i) {
-      if (num % i == 0) {
-        flag = true;
-        break;
+    System.out.println("Hi Smita");
       }
-    }
-    if (!flag)
-      System.out.println(num + " is a prime number.");
-    else
-      System.out.println(num + " is not a prime number.");
-  }
+    
 }
